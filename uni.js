5edcd1884fd24148964b01f55da99f5fdee21a4f
sujan poudel <spoudel347@gmail.com>
2017-11-24 10:42:59 +0545

    POST and GET  request filtration

--- uni.js
+++ uni.js
@@ -3,6 +3,12 @@ var http = require('http');
 var qs = require('querystring');
 
 http.createServer(function (req, res) {
+
+  if (req.method === 'POST') {
+    var body = '';
+    req.on('data', function(chunk) {
+      body += chunk;
+    });
     req.on('end', function() {
       var data = qs.parse(body);
       res.writeHead(200);
@@ -13,6 +19,9 @@ http.createServer(function (req, res) {
        console.log("converting with smartConvert="+data.smartConvert);
       console.log("send after converted"+converted+"|");
     });
-  
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
 
 }).listen(1234);
